c72a34281b2b2129b8396dd892b432574e340b13
nasfree <nfrman17@gmail.com>
2018-08-13 16:18:58 -0400

    changed arrayproperty back to stringproperty

--- dailyexpenses.py
+++ dailyexpenses.py
@@ -2,8 +2,8 @@ from google.appengine.ext import ndb
 
 class Food_Ex(ndb.Model):
     # date = ndb.DateProperty(required=True)
-    foods = ndb.ArrayProperty(required=True)
-    price1 = ndb.ArrayProperty(required=True)
+    foods = ndb.StringProperty(required=True)
+    price1 = ndb.StringProperty(required=True)
 
 class Transportation_Ex(ndb.Model):
     transportation = ndb.StringProperty(required=True)
