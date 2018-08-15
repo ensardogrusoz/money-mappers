bf0dd198125f6b2b198a395953efe16a3a86bf98
nasfree <nfrman17@gmail.com>
2018-08-15 01:51:58 -0400

    Got it to display every item

--- dailyexpenses.py
+++ dailyexpenses.py
@@ -1,30 +1,24 @@
 from google.appengine.ext import ndb
 
+
+class CssiUser(ndb.Model):
+  first_name = ndb.StringProperty(required=True)
+  last_name = ndb.StringProperty()
+
 class Date(ndb.Model):
     date = ndb.DateProperty(required=True)
 
 class Food_Ex(ndb.Model):
-    foods = ndb.StringProperty(repeated=True)
-    price1 = ndb.IntegerProperty(repeated=True)
+    food = ndb.StringProperty(required=True)
+    price1 = ndb.FloatProperty(required=True)
+    # ourusers = ndb.KeyProperty(CssiUser, repeated=True)
+    # price1 = ndb.IntegerProperty(repeated=True)
 
 class Transportation_Ex(ndb.Model):
     transportation = ndb.StringProperty(required=True)
-    price2 = ndb.IntegerProperty(required=True)
+    price2 = ndb.FloatProperty(required=True)
 
 class Entertainment_Ex(ndb.Model):
     entertainment = ndb.StringProperty(required=True)
-    price3 = ndb.IntegerProperty(required=True)
+    price3 = ndb.FloatProperty(required=True)
     # total = ndb.StringProperty(required=True)
-
-class CssiUser(ndb.Model):
-  """CssiUser stores information about a logged-in user.
-
-  The AppEngine users api stores just a couple of pieces of
-  info about logged-in users: a unique id and their email address.
-
-  If you want to store more info (e.g. their real name, high score,
-  preferences, etc, you need to create a Datastore model like this
-  example).
-  """
-  first_name = ndb.StringProperty()
-  last_name = ndb.StringProperty()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 from datetime import datetime
 
 
-from google.appengine.api import users
+
 from google.appengine.ext import ndb
 
 the_jinja_env = jinja2.Environment(
@@ -16,157 +16,246 @@ the_jinja_env = jinja2.Environment(
 
 
 class Login(webapp2.RequestHandler):
-  def get(self):
-    user = users.get_current_user()
-    # If the user is logged in...
-    if user:
-      first_name = user.nickname()
-      cssi_user = CssiUser.get_by_id(user.user_id())
-      signout_link_html = '<a href="%s">sign out</a>' % (
-          users.create_logout_url('/login'))
-      # If the user has previously been to our site, we greet them!
-      if cssi_user:
-        self.response.write('''
-            Welcome %s %s! <br> %s <br>''' % (
-              cssi_user.first_name,
-              cssi_user.last_name,
-              signout_link_html))
-      # If the user hasn't been to our site, we ask them to sign up
-      else:
-        self.response.write('''
-            Welcome to our site!  Please sign up! <br>
-            <form method="post" action="/mainpage">
-            <input type="text" name="first_name">
-            <input type="text" name="last_name">
-            <input type="submit">
-            </form><br> %s <br>
-            ''' % (signout_link_html))
-
-    # Otherwise, the user isn't logged in!
-    else:
-      self.response.write('''
-        Please log in to use our site! <br>
-        <a href="%s">Sign in</a>''' % (
-          users.create_login_url('/login')))
-
-  def post(self):
-    user = users.get_current_user()
-    if not user:
+    def get(self):
+      from google.appengine.api import users
+      user = users.get_current_user()
+      # If the user is logged in...
+      if user:
+        first = user.nickname()
+        cssi_user = CssiUser.get_by_id(user.user_id())
+        signout_link_html = '<a href="%s">sign out</a>' % (
+            users.create_logout_url('/'))
+        # If the user has previously been to our site, we greet them!
+        if cssi_user:
+          self.response.write('''
+              Welcome %s! <br> %s <br>''' % (
+                cssi_user.first_name,
+                signout_link_html))
+        # If the user hasn't been to our site, we ask them to sign up
+        else:
           self.response.write('''
               Welcome to our site!  Please sign up! <br>
               <form method="post" action="/mainpage">
-              <input type="text" name="first_name">
-              <input type="text" name="last_name">
+              <h3>Enter First Name: </h3><input type="text" name="first_name">
+              <h3>Enter Last Name: </h3><input type="text" name="last_name">
               <input type="submit">
               </form><br> %s <br>
               ''' % (signout_link_html))
-    cssi_user = CssiUser(
-        first_name=self.request.get('first_name'),
-        last_name=self.request.get('last_name'),
-        id=user.user_id())
-    cssi_user.put()
-    self.response.write('Thanks for signing up, %s!' %
-        cssi_user.first_name)
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        cssi_user = CssiUser(
+        cs_user = CssiUser(
             first_name=self.request.get('first_name'),
             last_name=self.request.get('last_name'),
             id=user.user_id())
-        cssi_user.put()
-        signout_link_html = '<a href="%s">sign out</a>' % (
-            users.create_logout_url('/login'))
-
-        if user:
-            user = users.get_current_user()
-            first_name = user.nickname()
-            main_template = the_jinja_env.get_template('template/mainpage.html')
-            variable_dic={
-                "first_name": first_name,
-            }
-            self.response.write('''
-                Welcome %s! <br> %s <br>''' % (
-                  cssi_user.first_name,
-                  signout_link_html))
-            self.response.write(main_template.render(variable_dic))
-        if cssi_user:
-          self.response.write('''
-              Welcome %s %s! <br> %s <br>''' % (
-                cssi_user.first_name,
-                cssi_user.last_name,
-                signout_link_html))
-        else:
-            self.response.write('''
-              Please log in to use this page! <br>
-              <a href="%s">Sign in</a>''' % (
-                users.create_login_url('/login')))
-
-          # You shouldn't be able to get here without being logged in
+        cs_user.put()
+      # Otherwise, the user isn't logged in!
+      else:
+        self.response.write('''
+          Please log in to use our site! <br>
+          <a href="%s">Sign in</a>''' % (
+            users.create_login_url('/login')))
 
     def post(self):
+      from google.appengine.api import users
+      user = users.get_current_user()
+      # If the user is logged in...
+      if user:
+        first = user.nickname()
+        cssi_user = CssiUser.get_by_id(user.user_id())
+        signout_link_html = '<a href="%s">sign out</a>' % (
+            users.create_logout_url('/'))
+        # If the user has previously been to our site, we greet them!
+        if cssi_user:
+          self.response.write('''
+              Welcome %s! <br> %s <br>''' % (
+                cssi_user.first_name,
+                signout_link_html))
+        # If the user hasn't been to our site, we ask them to sign up
+        else:
+          self.response.write('''
+              Welcome to our site!  Please sign up! <br>
+              <form method="post" action="/mainpage">
+              <h3>Enter First Name: </h3><input type="text" name="first_name">
+              <h3>Enter Last Name: </h3><input type="text" name="last_name">
+              <input type="submit">
+              </form><br> %s <br>
+              ''' % (signout_link_html))
+        cs_user = CssiUser(
+            first_name=self.request.get('first_name'),
+            last_name=self.request.get('last_name'),
+            id=user.user_id())
+        cs_user.put()
+      # Otherwise, the user isn't logged in!
+      else:
+        self.response.write('''
+          Please log in to use our site! <br>
+          <a href="%s">Sign in</a>''' % (
+            users.create_login_url('/login')))
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        from google.appengine.api import users
+        user = users.get_current_user()
+        signout_link_html = '<a href="%s">sign out</a>' % (
+            users.create_logout_url('/'))
+        # If the user is logged in...
+        if user:
+          first = user.nickname()
+          cssi_user = CssiUser.get_by_id(user.user_id())
+
+          # If the user has previously been to our site, we greet them!
+          if cssi_user:
+            cs_user = CssiUser(
+                first_name=self.request.get('first_name'),
+                last_name=self.request.get('last_name'),
+                id=user.user_id())
+            cs_user.put()
+            self.response.write('''
+                Welcome %s! <br> %s <br>''' % (
+                  cs_user.first_name,
+                  signout_link_html))
+            main_template = the_jinja_env.get_template('template/mainpage.html')
+            self.response.write(main_template.render())
+          else:
+            fail_template = the_jinja_env.get_template('template/main-fail.html')
+            self.response.write(fail_template.render())
+        # Otherwise, the user isn't logged in!
+        else:
+          fail_template = the_jinja_env.get_template('template/main-fail.html')
+          self.response.write(fail_template.render())
+
+    def post(self):
+        from google.appengine.api import users
         user = users.get_current_user()
         cssi_user = CssiUser.get_by_id(user.user_id())
+        cs_user = CssiUser(
+            first_name=self.request.get('first_name'),
+            last_name=self.request.get('last_name'),
+            id=user.user_id())
+        cs_user.put()
+        signout_link_html = '<a href="%s">Sign Out</a>' % (
+             users.create_logout_url('/'))
+        if cssi_user:
+          self.response.write('''
+              Welcome %s! <br> %s <br>''' % (
+                cs_user.first_name,
+                signout_link_html))
         main_template = the_jinja_env.get_template('template/mainpage.html')
-        self.response.write('''
-            Welcome %s! <br> %s <br>''' % (
-              cssi_user.first_name,
-              signout_link_html))
-        self.response.write(main_template.render())
+        date = self.request.get("date")
+        variable_dict = {
+            "date": date,
+        }
+        self.response.write(main_template.render(variable_dict))
+        wrappeddate = datetime.strptime(date, "%Y-%m-%d")
+        daTe = Date(date = wrappeddate)
+
 
 class Expenses(webapp2.RequestHandler):
     def get(self):
         self.response.write("This is the expense page")
     def post(self):
         expense_template = the_jinja_env.get_template('template/expense.html')
-        date = self.request.get("date")
-        foodList = []
-        food = self.request.get_all('user-in-1')
-        while(not food == ''):
-            foodList.append(food)
-        print(foodList)
-        # food_list.append(food)
-        price1 = self.request.get_all('user-in-2')
-        print price1
-        transportation = self.request.get_all('user-in-3')
+        food = self.request.get('user-in-1')
+        price1 = self.request.get('user-in-2')
+        transportation = self.request.get('user-in-3')
         price2 = self.request.get('user-in-4')
         entertainment = self.request.get('user-in-5')
         price3 = self.request.get('user-in-6')
-        # list = self.request.get('list')
-        # list2 = self.request.get('list2')
-        # list3 = self.request.get('list3')
-        variable_dict={
-            # 'food_info': food_list,
-            "date": date,
+        foods = Food_Ex.query().fetch()
+        trans = Transportation_Ex.query().fetch()
+        enter = Entertainment_Ex.query().fetch()
+        var_dic={
             "food": food,
             "price1": price1,
             "transportation": transportation,
             "price2": price2,
             "entertainment": entertainment,
             "price3": price3,
-            # "list": list,
-            # "list2": list2,
-            # "list3": list3,
+            'food_info' : foods,
+            'trans_info' : trans,
+            'enter_info' : enter,
         }
-        self.response.write(expense_template.render(variable_dict))
-        wrappeddate = datetime.strptime(date, "%Y-%m-%d")
-        daTe = Date(date = wrappeddate)
-        expenses = Food_Ex(foods = food, price1 = price1)
-        transport_ex = Transportation_Ex(transportation = transportation, price2 = price2)
-        entertain_ex = Entertainment_Ex(entertainment = entertainment, price3 = price3)
-        expenses.put()
+        self.response.write(expense_template.render(var_dic))
+        food_ex = Food_Ex(food = food, price1 = float(price1))
+        food_ex.put()
+        transport_ex = Transportation_Ex(transportation = transportation, price2 = float(price2))
         transport_ex.put()
+        entertain_ex = Entertainment_Ex(entertainment = entertainment, price3 = float(price3))
         entertain_ex.put()
+
+
 
 class Home(webapp2.RequestHandler):
     def get(self):
-        home_template = the_jinja_env.get_template('template/welcome.html')
-        self.response.write(home_template.render())
+        from google.appengine.api import users
+        user = users.get_current_user()
+        signout_link_html = '<a href="%s">sign out</a>' % (
+            users.create_logout_url('/'))
+        # If the user is logged in...
+        if user:
+          first = user.nickname()
+          cssi_user = CssiUser.get_by_id(user.user_id())
+          if cssi_user:
+              cs_user = CssiUser(
+                  first_name=self.request.get('first_name'),
+                  last_name=self.request.get('last_name'),
+                  id=user.user_id())
+              cs_user.put()
+              home_template = the_jinja_env.get_template('template/welcome.html')
+              self.response.write(home_template.render())
+          else:
+              home_template = the_jinja_env.get_template('template/welcome.html')
+              self.response.write(home_template.render())
+          # Otherwise, the user isn't logged in!
+        else:
+          home_template = the_jinja_env.get_template('template/welcome.html')
+          self.response.write(home_template.render())
 
+    def post(self):
+        from google.appengine.api import users
+        user = users.get_current_user()
+        signout_link_html = '<a href="%s">sign out</a>' % (
+            users.create_logout_url('/'))
+        # If the user is logged in...
+        if user:
+          first = user.nickname()
+          cssi_user = CssiUser.get_by_id(user.user_id())
+          if cssi_user:
+            cs_user = CssiUser(
+                first_name=self.request.get('first_name'),
+                last_name=self.request.get('last_name'),
+                id=user.user_id())
+            cs_user.put()
+            home_template = the_jinja_env.get_template('template/welcome.html')
+            self.response.write(home_template.render())
+          else:
+            home_template = the_jinja_env.get_template('template/welcome.html')
+            self.response.write(home_template.render())
+      # Otherwise, the user isn't logged in!
+        else:
+          home_template = the_jinja_env.get_template('template/welcome.html')
+          self.response.write(home_template.render())
+
+class Start(webapp2.RequestHandler):
+    def get(self):
+        start_template = the_jinja_env.get_template('template/main.html')
+        self.response.write(start_template.render())
+
+    def post(self):
+        start_template = the_jinja_env.get_template('template/main.html')
+        self.response.write(start_template.render())
+
+class Monthly(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Monthly Expense Page")
+
+    def post(self):
+        month_template = the_jinja_env.get_template('template/monthly.html')
+        self.response.write(month_template.render())
+        
 app = webapp2.WSGIApplication([
   ('/', Home),
   ('/login', Login),
+  ('/start', Start),
   ('/mainpage', MainPage),
-  ('/expense', Expenses)
+  ('/expense', Expenses),
+  ('/monthly', Monthly)
 ], debug=True)
